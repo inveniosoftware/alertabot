ae46fddaf37527574e2d1dabb90e8504e3c0410a
Antonio Vivace <avivace4@gmail.com>
2022-10-27 15:32:52 +0200

    Harbor: basic unpacking of the Scanning completed event

--- src/main.py
+++ src/main.py
@@ -23,7 +23,21 @@ def generate_hash_signature(
     return hmac.new(secret, payload, digest_method).hexdigest()
 
 
-@app.post("/webhook/7938421", status_code=http.HTTPStatus.ACCEPTED)
+def parse_harbor_event(payload):
+    event = {
+        "type": payload["type"],
+        "repo": payload["event_data"]["repository"]["repo_full_name"],
+    }
+
+    if event["type"] == "SCANNING_COMPLETED":
+        scan_overview = payload["event_data"]["resources"][0]["scan_overview"]
+        scan = list(scan_overview.values())[0]
+        event["severity"] = scan["severity"]
+        event["summary"] = scan["summary"]
+    return event
+
+
+@app.post("/webhook/1", status_code=http.HTTPStatus.ACCEPTED)
 async def webhook(request: Request, x_hub_signature: str = Header(None)):
     payload = await request.body()
     secret = os.environ.get("WEBHOOK_SECRET").encode("utf-8")
@@ -32,13 +46,28 @@ async def webhook(request: Request, x_hub_signature: str = Header(None)):
     # Decode incoming request
     json_payload = json.loads(payload)
 
-    print(json_payload)
-    
+    event = parse_harbor_event(json_payload)
+
+    msg_content = f"""
+    Event **{event["type"]}** coming from **{event["repo"]}**
+    """
+
+    if event["type"] == "SCANNING_COMPLETED":
+        vuln_embed = {
+            "title": "Vulnerabilities found",
+            "color": 16711680,
+            "description": f"{event['summary']['total']} total vulnerabilities ({event['summary']['fixable']} fixable), {event['summary']['summary']['High']} high severity.",
+        }
+        embeds = [vuln_embed]
+    else:
+        embeds = []
+
     # Prepare payload to post to Discord
     message = {
         "username": "Harbor",
         "avatar_url": "https://goharbor.io/img/logos/harbor-icon-color.png",
-        "content": f'Event {json_payload["type"]} coming from TODO',
+        "content": msg_content,
+        "embeds": embeds,
     }
 
     # Post to the target webhook
